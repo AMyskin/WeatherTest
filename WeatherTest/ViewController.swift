e4d0f431405f27f8488c8091fdb261428bebd525
Aleksander Myskin <Amyskin@yandex.ru>
2025-05-13 12:38:01 +0300

    Initial Commit

---
+++ WeatherTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Alexander Myskin on 13.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
